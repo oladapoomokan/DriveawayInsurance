c3489209963873a694527a4ce5204c6c32f4fc80
Dapz Baba <Dapz Baba@LAPTOP-93RAETT3.home>
2019-03-31 15:12:23 +0100

    DriveawayInsurance

---
+++ EdurekaProject-31032019/src/Day1/HelloWorld.java
@@ -0,0 +1,17 @@
+package Day1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		Acceptance Criteria
+		Scenario: The registered user should be able to check if vehicle exists.
+		Given I am on the car insurance website.
+		When I type in Car registration OV12UYY in “Enter Reg” search field and I click on find vehicle.
+		Then I get results of cover start date, time and cover end date and time.
+
+system.out.println("Acceptance Criteria"
+		+ "Scenario: The registered user should be able to check if vehicle exists."
+		+ "Given I am on the car insurance website."
+		+ "When I type in the car registration OV12UYY IN ENTER Reg search field and I click on find vehicle ."
+		+ "Then I should get results of cover start date and time and cover end and time.);
